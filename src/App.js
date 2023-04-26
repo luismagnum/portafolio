10e69d1f15911f4cd5284fc332e1e4ce1bbbdd37
luismagnum <luismagnum1@gmail.com>
2023-04-26 01:09:39 -0300

    add proyects

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import git from './assets/images/git.png';
 import teque from './assets/images/teque.svg';
 import reactfoter from './assets/images/reactfoter.svg';
 import tailwfo from './assets/images/tailwfo.svg';
+import speedga from './assets/images/speedga.svg';
 
 
 function App() {
@@ -48,7 +49,7 @@ function App() {
         <section className='grid place-items-center justify-center pt-12' id='Seccion2'>
         <h2 className='text-white text-3xl font-bold py-2'>PROYECTS</h2>
         </section>
-         <div className='grid grid-cols-1 ml-2 odd:md:grid-cols-2 lg:grid-cols-3 gap-8'>
+         <div className='grid grid-cols-1 ml-2 odd:md:grid-cols-2 lg:grid-cols-4 gap-6'>
        <section className='bg-#011d41 h-screen flex justify-center items-center gap-x-16 text-white'>
        <div className='w-[300px] h-[420px] bg-transparent cursor-pointer group perspective'>
         <div className='relative preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-1000'>
@@ -112,6 +113,30 @@ function App() {
          </div>
         </div>
       </section>
+
+      <section className='bg-#011d41 h-screen flex justify-center items-center gap-x-16 text-white'>
+       <div className='w-[300px] h-[420px] bg-transparent cursor-pointer group perspective'>
+        <div className='relative preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-1000'>
+         <div className='absolute backface-hidden border-2 w-full'>
+          <img src={speedga} alt='' className='w-full h-full'/>
+          </div>
+           <div className='absolute my-rotate-y-180 backface-hidden w-full h-full bg-gray-100 overflow-hidden'>
+            <div className='text-center flex flex-col items-center justify-center h-full text-gray-800 px-2 pb-24'>
+             <h1 className='text-3xl font-semibold'>ECOMMERCE</h1>
+              <p className='my-2'>SpeedGamers</p>
+              <p>HTML y CSS</p>
+               <button className='bg-teal-500 px-6 py-2 font-semibold text-white rounded-full absolute -bottom-20 delay-500 duration-1000 group-hover:bottom-20 scale-0 group-hover:scale-125'>
+               <a href='https://www.speedgamers.online'>
+                WEB
+               </a>
+               </button>
+            </div>
+           </div>
+         </div>
+        </div>
+      </section>
+
+
       <div className='grid place-items-center justify-center w-screen '>
       <section className='flex items-center justify-center' id='Seccion3'>
       <h2 className=' text-white text-3xl font-bold mt-8'>CONTACT</h2>
