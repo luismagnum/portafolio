40f66b1fdf29c2b21a4bb067fb8202e94ebd5214
luismagnum <luismagnum1@gmail.com>
2024-04-02 23:48:55 -0300

    change proyects

--- src/components/Proyects.js
+++ src/components/Proyects.js
@@ -5,6 +5,7 @@ import teque from '../assets/images/teque.svg';
 import speedga from '../assets/images/speedga.svg';
 import movi from '../assets/images/movi.svg';
 import tequenos from '../assets/images/images/tequenos.svg';
+import dulces from '../assets/images/images/dulcesE.png';
 
 export default function Proyects() {
   return (
@@ -123,15 +124,15 @@ export default function Proyects() {
        <div className='w-[300px] h-[420px] bg-transparent cursor-pointer group perspective'>
         <div className='relative preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-1000'>
          <div className='absolute backface-hidden border-2 w-full'>
-          <img src={tequenos} alt='' className='w-full h-full'/>
+          <img src={dulces} alt='' className='w-full h-full'/>
           </div>
            <div className='absolute my-rotate-y-180 backface-hidden w-full h-full bg-gray-100 overflow-hidden'>
             <div className='text-center flex flex-col items-center justify-center h-full text-gray-800 px-2 pb-24'>
-             <h1 className='text-3xl font-semibold'>Tequeños Vzlanos</h1>
+             <h1 className='text-3xl font-semibold'>Dulces</h1>
               <p className='my-2'>ECOMMERCE</p>
-              <p>HTML y CSS</p>
+              <p>REACT NEXTJS14</p>
                <button className='bg-[#011d41] px-6 py-2 font-semibold text-white rounded-full absolute -bottom-20 delay-500 duration-1000 group-hover:bottom-20 scale-0 group-hover:scale-125'>
-               <a href='https://www.tequevzla.online'target='_blank' rel="noreferrer">
+               <a href='https://dulces-wnyp.vercel.app'target='_blank' rel="noreferrer">
                 WEB
                </a>
                </button>
