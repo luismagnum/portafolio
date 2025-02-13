b8b3f4490156d4a3219f776a03f96fe68720741e
luismagnum <luismagnum1@gmail.com>
2025-02-13 00:09:05 -0300

    foter

--- src/components/Foter.js
+++ src/components/Foter.js
@@ -7,7 +7,7 @@ export default function Foter() {
   return (
     <footer className="bg-[#0C0C0C] text-gray-300 py-10 shadow-lg mt-8 text-center">
       <p className="text-lg md:text-xl font-semibold text-teal-500">
-        Dev. Luis E. Diaz
+        Devs. Luis E. Diaz
       </p>
 
       <div className="flex justify-center items-center gap-6 mt-4">
